99bc9f74f0d2674f014eb4bf0cf9f3baedb3f326
otolaa <kalabunga82@gmail.com>
2025-02-17 23:59:27 +0200

    🤑

--- README.md
+++ README.md
@@ -4,4 +4,16 @@
 go mod init todo
 go get github.com/aquasecurity/table
 go run ./
+
+```
+
+## Command CLI for todo
+
+```
+go run ./ -help
+go run ./ -add "Title for item todo"
+go run ./ -del 1
+go run ./ -edit int:"New string"
+go run ./ -list
+
 ```
--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 func main() {
 	todos := Todos{}
-	todos.add("Buy Milk")
-	todos.add("Buy Bread")
-	todos.toggle(0)
-	todos.print()
+	storage := NewStorage[Todos]("todos.json")
+	storage.Load(&todos)
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
+	storage.Save(todos)
 }
